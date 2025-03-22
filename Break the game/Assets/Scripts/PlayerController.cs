34105f6c3c01a2ebae58f75b4d8c546818a67777
CloudyNo <noahagdam@gmail.com>
2025-03-22 17:36:38 +0100

    Shitbox movement fix ._.

--- Break the game/Assets/Scripts/PlayerController.cs
+++ Break the game/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Player attributes
+    [Header("Movement Parameters")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float gravity;
+
+    [Header("Multiple Jumps")]
+    [SerializeField] private int extraJumps;
+    private int jumpCounter;
+
+    [Header("Coyote Time")]
     [SerializeField] private float coyoteTime;
-    private bool canDoubleJump;
+    private float coyoteCounter;
+
+    // Collider stuff
+    [Header("Layers Interactions")]
     [SerializeField] private LayerMask groundLayer;
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb;
+    private float horizontalInput;
 
     
     // Executes on startup
@@ -25,25 +38,86 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         // Horizontal and vertical movement according to input
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
 
+        // Flip player according to movement direction
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         // Check Jump input
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetAxis("Vertical") > 0.3)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             Jump();
         }
+
+        // Adjust Jump height if key isnt held down anymore
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y / 2);
+        }
+
+        // Reset gravity
+        rb.gravityScale = gravity;
+
+        // Horizontal movement is performed here
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        if (isGrounded())
+        {
+            coyoteCounter = coyoteTime; //Reset coyote counter when on the ground
+            jumpCounter = extraJumps; //Reset jump counter to extra jump value
+        }
+        else
+        {
+            coyoteCounter -= Time.deltaTime; 
+        }
+        
     }
 
     // Player functions
     private void Jump()
     {   
+        // Can you still jump???
+        if (coyoteCounter <= 0 && jumpCounter <= 0)
+        {
+            return;
+        }
+
+        
         if (isGrounded())
         {
-            canDoubleJump = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
         }
+        else
+        {
+            // If not on ground and coyoteCounter ran out, do a normal jump instead of an extra jump
+            if (coyoteCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            }
+            else 
+            {
+                if (jumpCounter > 0) // Check if we still have jumps left to do
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+                    jumpCounter--;
+                }
+            }
+            // Reset coyoteCounter
+            coyoteCounter = 0;
+        }
+
+        
+        
     }
+
+    
 
     private bool isGrounded()
     {
@@ -51,4 +125,3 @@ public class PlayerController : MonoBehaviour
         return raycastHit.collider != null;
     }
 }
-
