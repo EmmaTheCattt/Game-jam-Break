7e9d8333aa4c53f5c2013ae8bbe868b1d2535aba
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-03-22 16:42:56 +0100

    Clouds done

--- Break the game/Assets/Prefab/Clouds/Cloud_logic.cs
+++ Break the game/Assets/Prefab/Clouds/Cloud_logic.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
 public class Cloud_logic : MonoBehaviour
 {
@@ -11,8 +13,16 @@ public class Cloud_logic : MonoBehaviour
     public float speed;
     public float size;
     public float time = 0;
+    public float alpha = 1;
+    public Color C;
+
+    public GameObject Cloud;
+    public SpriteRenderer SR;
     void Start()
     {
+        Cloud = this.gameObject;
+        SR = Cloud.GetComponent<SpriteRenderer>();
+        Color C = SR.material.color;
         Cloud_transform = GetComponent<Transform>();
         speed = Random.Range(-0.5f, -1.5f);
         size = Random.Range(-0.25f, 0.25f);
@@ -20,16 +30,41 @@ public class Cloud_logic : MonoBehaviour
 
         movement = new Vector3(speed, 0, 0);
         Invoke("Clouds_death", 20f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += movement * Time.deltaTime;
+        C.a = alpha;
+        SR.material.color = C;
     }
 
     private void Clouds_death()
     {
-        Destroy(gameObject);
+        InvokeRepeating("fade", 0.05f, 0.05f);
+        if (alpha < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void fade()
+    {
+        alpha -= 0.01f;
+    }
+
+    private void Fast_fade()
+    {
+        alpha -= 0.025f;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Cursor"))
+        {
+            InvokeRepeating("Fast_fade", 0.05f, 0.05f);
+        }
     }
 }
