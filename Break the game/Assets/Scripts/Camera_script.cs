2cd6f8261e5e7c189393a02b7193dcac33adc0af
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-03-22 19:00:20 +0100

    push

--- Break the game/Assets/Scripts/Camera_script.cs
+++ Break the game/Assets/Scripts/Camera_script.cs
@@ -8,8 +8,11 @@ public class Camera_script : MonoBehaviour
     public GameObject player;
     public GameObject end;
 
+    public Vector3 Camera_pos;
+
     public float camera_x;
     public float camera_y;
+    public float offset = 5;
     void Start()
     {
         camera_y = transform.position.y;
@@ -20,6 +23,15 @@ public class Camera_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.x <= end.transform.position.x + offset)
+        {
+            Camera_pos = new Vector3(end.transform.position.x + offset, camera_y, -1);
+        }
+        else
+        {
+            Camera_pos = new Vector3(player.transform.position.x, camera_y, -1);
+        }
         
+        transform.position = Camera_pos;
     }
 }
